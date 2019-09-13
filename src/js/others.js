e13fb17419eb67d61e967ef5610cb539fad57115
Thusitha Wijethunga <thusithawijethunga@gmail.com>
2019-09-13 15:14:45 +0530

    error pointer.endX  for mobile
    error pointer.endX  for mobile

--- src/js/others.js
+++ src/js/others.js
@@ -130,8 +130,15 @@ export default {
   change(event) {
     const { options, pointers } = this;
     const pointer = pointers[Object.keys(pointers)[0]];
-    const offsetX = pointer.endX - pointer.startX;
-    const offsetY = pointer.endY - pointer.startY;
+
+
+    if (pointer) {
+      var offsetX = pointer.endX - pointer.startX;
+      var offsetY = pointer.endY - pointer.startY;
+    } else {
+      var offsetX = 0;
+      var offsetY = 0;
+    }
 
     switch (this.action) {
       // Move the current image
